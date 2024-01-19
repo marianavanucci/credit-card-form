c3658a215e05392b54234a23ad0da3a95b158938
Mariana Vanucci <marianavanucciandrade@gmail.com>
2024-01-19 18:30:21 -0300

    setting Space Grotesk font

---
+++ components/CardInput.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+const CardInput = () => {
+
+    const styles = {
+        container: [
+          "bg-white w-96 pb-20 rounded-tl-[20px]", 
+          "rounded-tr-[20px] rounded-br-[120px] rounded-bl-[20px]"
+        ]
+      }
+
+  return (
+    <div className='styles.container'>CardInput</div>
+  )
+}
+
+export default CardInput
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,6 @@ module.exports = {
           300: '#21092f',
         }
       },
-
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
